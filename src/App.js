4720152b84250257e52b93110cfaf38baba3cd8a
Abel Souza <abelsouzacosta@gmail.com>
2021-07-15 19:25:00 -0300

    style: CharacterCard, App

--- src/App.js
+++ src/App.js
@@ -5,41 +5,36 @@ import { CharacterCard } from "./components/CharacterCard";
 
 class App extends Component {
   state = {
-    results: [
-      {
-        id: 1,
-        name: "Ricky Sanchez",
-        status: "Alive",
-        gender: "Male",
-        species: "Human",
-        origin: {
-          name: "Earth",
-        },
-        location: {
-          name: "Earth, Replacement Universe",
-        },
-        image: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
-      },
-    ],
+    results: [],
   };
 
-  // componentDidMount() {
-  //   fetch(`https://rickandmortyapi.com/api/character`)
-  //     .then((response) => response.json())
-  //     .then((result) =>
-  //       this.setState({
-  //         results: result.results,
-  //       })
-  //     );
-  // }
+  componentDidMount() {
+    fetch(`https://rickandmortyapi.com/api/character`)
+      .then((response) => response.json())
+      .then((result) =>
+        this.setState({
+          results: result.results,
+        })
+      );
+  }
 
   render() {
     return (
-      <div className="App">
-        {this.state.results.length > 0 &&
-          this.state.results.map((el) => {
-            return <CharacterCard character={el} />;
-          })}
+      <div className="container">
+        <div className="page-title">
+          <h1>The Ricky and Morty API</h1>
+        </div>
+
+        <div className="grid-container">
+          {this.state.results.length > 0 &&
+            this.state.results.map((el) => {
+              return (
+                <div className="grid-item">
+                  <CharacterCard character={el} />
+                </div>
+              );
+            })}
+        </div>
       </div>
     );
   }
