518b324cd544e3631fd960615a260cac74b710a8
Abel Souza <abelsouzacosta@gmail.com>
2021-07-15 18:57:06 -0300

    Estilizando componente de card

--- src/components/CharacterCard/index.jsx
+++ src/components/CharacterCard/index.jsx
@@ -3,11 +3,36 @@ import React from "react";
 import "./styles.css";
 
 export const CharacterCard = ({ character }) => {
-  const { name, status, gender, species, image } = character;
+  const { name, status, gender, species, image, location, origin } = character;
 
   return (
     <div className="character-profile">
       <img src={image} alt="name" />
+
+      <div className="character-general-info">
+        <div className="character-profile__title">
+          <h2>{name}</h2>
+        </div>
+
+        <span className="character-status">
+          <span className="status__icon"></span>
+          {status}
+        </span>
+
+        <div className="character-profile__personal-info">
+          <p>
+            Gender: {gender}, {species}
+          </p>
+        </div>
+
+        <div className="character-profile__origin">
+          <p>Origin: {origin.name}</p>
+        </div>
+
+        <div className="character-profile__location">
+          <p>Location: {location.name}</p>
+        </div>
+      </div>
     </div>
   );
 };
